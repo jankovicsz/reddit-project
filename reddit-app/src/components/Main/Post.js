9ae30385328e7467c33dbc1440a030ae46a0beed
Jankovics Zoltán <jankovicsz@gmail.com>
2021-03-10 21:19:49 +0100

    Update layout color design

--- reddit-app/src/components/Main/Post.js
+++ reddit-app/src/components/Main/Post.js
@@ -1,20 +1,23 @@
 export default function Post() {
   return (
     <div className='post'>
-      <h2>Ez egy post</h2>
-      <div>
-      <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ad
-        adipisci praesentium explicabo doloribus facere maiores possimus, at sit
-        fugit quis dignissimos sequi cupiditate temporibus modi molestias
-        quaerat rem quo.
-      </p>
-      <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ad
-        adipisci praesentium explicabo doloribus facere maiores possimus, at sit
-        fugit quis dignissimos sequi cupiditate temporibus modi molestias
-        quaerat rem quo.
-      </p>
+      <div className='vote'>
+        1
+      </div>
+      <div className='content'>
+        <h2>Ez egy post</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ad
+          adipisci praesentium explicabo doloribus facere maiores possimus, at
+          sit fugit quis dignissimos sequi cupiditate temporibus modi molestias
+          quaerat rem quo.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ad
+          adipisci praesentium explicabo doloribus facere maiores possimus, at
+          sit fugit quis dignissimos sequi cupiditate temporibus modi molestias
+          quaerat rem quo.
+        </p>
       </div>
     </div>
   );
